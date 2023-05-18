1a75eec2536cd7cb2808ffc71ab0e695a5ce9506
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-18 10:27:36 -0400

    🛠️ Updates router and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import NavBar from "./components/global/NavBar";
+import Footer from "./components/global/Footer";
 import Home from "./pages/Home";
 import Index from "./pages/Index";
 import New from "./pages/New";
 import Show from "./pages/Show";
 import Edit from "./pages/Edit";
+import PageWrapper from "./components/global/PageWrapper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PageNotFound from "./pages/PageNotFound";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
-      <Router>
-        <main>
+      <QueryClientProvider client={queryClient}>
+        <Router>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<Index />} />
-            <Route path="/books/new" element={<New />} />
-            <Route path="/books/:id" element={<Show />} />
-            <Route path="/books/:id/edit" element={<Edit />} />
-          </Routes>
+          <PageWrapper>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/books" element={<Index />} />
+              <Route path="/books/new" element={<New />} />
+              <Route path="/books/:id" element={<Show />} />
+              <Route path="/books/:id/edit" element={<Edit />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </PageWrapper>
           <Footer />
-        </main>
-      </Router>
+        </Router>
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,28 @@
-import BookLover from "../components/Booklover";
+import { Link } from "react-router-dom";
+import BookLover from "../components/BookLoverSVG";
 export default function Home() {
   return (
-    <section className="mt-16 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
-      <div className="space-y-4 flex-1 sm:text-center lg:text-left">
-        <h1 className="text-gray-800 font-bold text-4xl xl:text-5xl">
+    <section className="mx-auto mt-16 max-w-screen-xl items-center px-4 pb-12 md:px-8 lg:flex">
+      <div className="flex-1 space-y-4 sm:text-center lg:text-left">
+        <h1 className="text-4xl font-bold text-gray-800 xl:text-5xl">
           One place to track all your
           <span className="text-pink-400"> Reading</span>
         </h1>
-        <p className="text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
+        <p className="max-w-xl leading-relaxed text-gray-600 sm:mx-auto lg:ml-0">
           The perfect tracker for every type of reader. Track the books you've
           read, your current read, and all your future reads in one place.
         </p>
-        <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
-          <a
-            href="javascript:void(0)"
-            className="px-7 py-3 w-full bg-pink-400 text-white text-center rounded-md block sm:w-auto"
+        <div className="items-center justify-center space-y-3 pt-10 sm:flex sm:space-x-6 sm:space-y-0 lg:justify-start">
+          <Link
+            to="books"
+            className="block w-full rounded-md bg-pink-400 px-7 py-3 text-center text-white sm:w-auto"
           >
-            Try it out
-          </a>
+            Try it
+          </Link>
         </div>
       </div>
-      <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
-        <BookLover className="w-full mx-auto sm:w-10/12  lg:w-full " />
+      <div className="mt-7 flex-1 text-center lg:ml-3 lg:mt-0">
+        <BookLover className="mx-auto w-full sm:w-10/12  lg:w-full " />
       </div>
     </section>
   );
