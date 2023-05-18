d59456e2af0ee47c6f48775ae340a1edebb0b804
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-18 16:21:57 -0400

    🎨 Fixes layout

--- src/components/global/PageWrapper.tsx
+++ src/components/global/PageWrapper.tsx
@@ -4,9 +4,9 @@ import NavBar from "./NavBar";
 export default function PageWrapper({ children }) {
   return (
     <>
-      <div className="relative min-h-screen">
+      <div className="relative h-full min-h-screen">
         <NavBar />
-        <main className=" px-16 pt-16">
+        <main className="px-16 pb-20 pt-20">
           <div className=" mx-auto max-w-screen-2xl px-4 md:px-8">
             {children}
           </div>
