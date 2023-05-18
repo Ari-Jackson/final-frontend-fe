08479486b3efb71a74b3f3480efa95bae477fc31
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-18 16:26:48 -0400

    🛠️ Replaces queries with hooks

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,24 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
 import FilterButton from "../components/FilterButton";
 import { Link } from "react-router-dom";
 import { BsFillSuitHeartFill } from "react-icons/bs";
+import useAllBooks from "../hooks/queries/useAllBooks";
 
 export default function Index() {
-  const API = import.meta.env.VITE_API_URL;
-  const {
-    isLoading,
-    isError,
-    data: books,
-  } = useQuery({
-    queryKey: ["books"],
-    queryFn: async () => {
-      const response = await fetch(`${API}/books`);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
-  });
+  const { isLoading, isError, data: books } = useAllBooks();
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -27,8 +13,6 @@ export default function Index() {
   if (isError) {
     return <span>There was an error</span>;
   }
-
-  console.log(books);
   return (
     <>
       <div className="mb-5 items-start justify-between border-b py-4 md:flex">
@@ -41,7 +25,10 @@ export default function Index() {
       </div>
       <div className="-m-4 flex flex-wrap">
         {books.map((book: any) => (
-          <div className="rounded-md p-2 duration-100 hover:bg-gray-50 lg:w-1/3">
+          <div
+            key={book.id}
+            className="rounded-md p-2 duration-100 hover:bg-gray-50 lg:w-1/3"
+          >
             <Link to={`${book.id}`}>
               <div className="flex h-full flex-col items-center justify-center text-center sm:flex-row sm:justify-start sm:text-left">
                 <div className="relative">
--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -1,25 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router-dom";
 import { BsFillBookmarkCheckFill, BsChatHeart } from "react-icons/bs";
 import { AiOutlineUnorderedList } from "react-icons/ai";
+import useSingleBook from "../hooks/queries/useSingleBook";
 
 export default function Show() {
-  const API = import.meta.env.VITE_API_URL;
   const { id } = useParams();
-  const {
-    isLoading,
-    isError,
-    data: book,
-  } = useQuery({
-    queryKey: ["books", id],
-    queryFn: async () => {
-      const response = await fetch(`${API}/books/${id}`);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
-  });
+  const { isLoading, isError, book } = useSingleBook(id);
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -90,7 +76,6 @@ const BreadCrumb = ({ title }: { title: string }) => {
         <li>
           <Link to="/books" className="block transition hover:text-gray-700">
             <span className="sr-only"> Home </span>
-
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
@@ -99,9 +84,9 @@ const BreadCrumb = ({ title }: { title: string }) => {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
               />
             </svg>
@@ -116,9 +101,9 @@ const BreadCrumb = ({ title }: { title: string }) => {
             fill="currentColor"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </li>
