576875ed41ede237e4d31a4205e897868e30e908
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-19 18:29:43 -0400

    🛠️ Adds input validation for user to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -25,7 +25,11 @@ export default function Form({
   values?: Inputs;
   onSubmitFunc: SubmitHandler<Inputs>;
 }) {
-  const { register, handleSubmit } = useForm<Inputs>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm<Inputs>({
     defaultValues: {
       title: "",
       genre: "",
@@ -35,11 +39,11 @@ export default function Form({
       wasCompletedBefore: false,
     },
     values,
+    mode: "onTouched",
   });
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     onSubmitFunc(data);
   };
-
   return (
     <form
       className="m-auto mt-10 w-full max-w-lg border px-10 py-10"
@@ -51,25 +55,49 @@ export default function Form({
       py-2 text-gray-700"
         type="text"
         placeholder="Book Title"
-        {...register("title", { required: true, maxLength: 500 })}
+        {...register("title", {
+          required: "Title is required",
+          maxLength: { value: 500, message: "Title is too long" },
+        })}
       />
-
+      {errors.title && (
+        <div className="text-normal mb-3 text-red-500 ">
+          {errors.title.message}
+        </div>
+      )}
       <label className="font-medium text-gray-600">Book Genre</label>
       <input
         type="text"
         className="w-full rounded border border-solid border-gray-300 px-4
     py-2 text-gray-700"
         placeholder="Book Genre"
-        {...register("genre", { required: true, maxLength: 255 })}
+        {...register("genre", {
+          required: "Genre is required",
+          maxLength: { value: 255, message: "Genre is too long" },
+        })}
       />
+      {errors.genre && (
+        <div className="text-normal mb-3 text-red-500 ">
+          {errors.genre.message}
+        </div>
+      )}
       <label className="font-medium text-gray-600">Rating</label>
       <input
         type="number"
         className="w-full rounded border border-solid border-gray-300 px-4
     py-2 text-gray-700"
-        placeholder="Rating"
-        {...register("rating", { max: 10, min: 0, valueAsNumber: true })}
+        placeholder="0"
+        {...register("rating", {
+          max: { value: 10, message: "Rating cannot be higher than 10" },
+          min: { value: 0, message: "Rating cannot be lower than 0" },
+          valueAsNumber: true,
+        })}
       />
+      {errors.rating && (
+        <div className="text-normal mb-3 text-red-500 ">
+          {errors.rating.message}
+        </div>
+      )}
       <input
         type="checkbox"
         placeholder="Is this a favorite>"
@@ -88,9 +116,9 @@ export default function Form({
         {...register("wasCompletedBefore", {})}
       />
       <label className="font-medium text-gray-600">Read Before</label>
-
       <input
-        className="text-md mt-4 w-full rounded border bg-pink-400 px-6 py-3 font-semibold text-white shadow xl:hover:bg-pink-200 xl:hover:text-pink-400"
+        disabled={!isDirty || !isValid}
+        className=" text-md mt-4 w-full rounded border bg-pink-400 px-6 py-3 font-semibold text-white shadow disabled:cursor-not-allowed disabled:bg-gray-200 disabled:hover:bg-gray-200 disabled:hover:text-white xl:hover:bg-pink-200 xl:hover:text-pink-400"
         type="submit"
       />
     </form>
