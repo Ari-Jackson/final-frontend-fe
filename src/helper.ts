81b3f9cec528ed41488a57d758669b643d1d3462
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-19 09:38:04 -0400

    🛠️ ✨ Creates  case changing function ✨ 🛠️

---
+++ src/helper.ts
@@ -0,0 +1,18 @@
+import { camelCase, snakeCase } from "change-case";
+import { type Inputs, type Outputs } from "./components/Form";
+
+export function caseChanger<T extends Inputs | Outputs>(
+  args: T,
+  endingType?: "camelCase" | "snakeCase"
+): T extends Inputs ? Outputs : Inputs {
+  const mapped = Object.entries(args).map((keyValue) => [
+    endingType === "camelCase"
+      ? camelCase(keyValue[0])
+      : snakeCase(keyValue[0]),
+    keyValue[1],
+  ]);
+
+  const mappedAsObj = Object.fromEntries(mapped);
+
+  return mappedAsObj;
+}
